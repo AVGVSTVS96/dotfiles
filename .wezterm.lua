093fe583025082989491a2f6d1de0d3d3c278cde
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-08-24 00:11:32 -0400

    Add and stow wezterm.lua config

---
+++ .wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("MesloLGS NF")
+config.font_size = 13
+config.color_scheme = "Tokyo Night"
+
+config.default_cursor_style = "SteadyUnderline"
+
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
+
+config.command_palette_bg_color = "#1A1B26"
+config.command_palette_fg_color = "#C0CAF5"
+
+config.max_fps = 120
+
+return config
