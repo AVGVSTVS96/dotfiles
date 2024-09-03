9cec6fe8276783dd24221cec96f52eda7f8efd93
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-09-02 20:08:31 -0400

    feat(wezterm): Update font to Monaspace Krypton
    WezTerm ships Nerd Fonts symbols by default!

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("MesloLGS NF")
+config.font = wezterm.font("Monaspace Krypton")
 config.font_size = 13
 config.color_scheme = "Tokyo Night"
 
