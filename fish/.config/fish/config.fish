a121f3697fdedce27e1d2b5bf50c93545b7174d0
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2025-12-01 10:15:00 +0000

    update gitignore and fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,10 @@
+# Environment variables
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx XDG_CONFIG_HOME ~/.config
+set -gx XDG_DATA_HOME ~/.local/share
+set -gx XDG_CACHE_HOME ~/.cache
+
 # Homebrew
 eval (/opt/homebrew/bin/brew shellenv)
 
@@ -7,6 +14,46 @@ oh-my-posh init fish --config $(brew --prefix oh-my-posh)/themes/tokyonight_stor
 # fzf
 #eval "$(fzf --fish)"
 fzf --fish | source
+
+# NPM local modules (highest priority for project-specific tools)
+fish_add_path -p ./node_modules/.bin
+
+# User-local binaries
+fish_add_path -p ~/.local/bin
+
+# NVM (Node Version Manager) - find and use latest/current version
+set -gx NVM_DIR $HOME/.nvm
+if test -d $NVM_DIR/versions/node
+  # Get the currently active or latest node version
+  if test -e $NVM_DIR/alias/default
+    set -l node_version (cat $NVM_DIR/alias/default)
+    set -l node_path $NVM_DIR/versions/node/(ls $NVM_DIR/versions/node | grep -F $node_version | head -1)
+  else
+    set -l node_path (ls -t $NVM_DIR/versions/node | head -1)
+    set node_path $NVM_DIR/versions/node/$node_path
+  end
+  
+  if test -d "$node_path"
+    fish_add_path $node_path/bin
+  end
+end
+
+# pnpm
+set -gx PNPM_HOME $HOME/Library/pnpm
+fish_add_path $PNPM_HOME
+
+# Cargo (Rust)
+fish_add_path $HOME/.cargo/bin
+
+# Cursor Editor
+fish_add_path "/Applications/Cursor.app/Contents/Resources/app/bin"
+
+# Bun
+fish_add_path "/Users/bassimshahidy/.cache/.bun/bin"
+
+# Additional environment variables
+set -gx BAT_THEME tokyonight_night
+set -gx LG_CONFIG_FILE $HOME/.config/lazygit/config.yml
 
 
 # Use bat for files, eza for directories
@@ -25,6 +72,12 @@ abbr frc "nvim ~/.config/fish/config.fish"
 abbr sfrc "source ~/.config/fish/config.fish"
 abbr exf "exec fish"
 abbr cl clear
+
+# -- cd abbreviations --
+abbr - "z -"
+abbr ... "cd ..."
+abbr .... "cd ....."
+abbr ..... "cd ......"
 
 # -- git aliases --
 abbr g git
